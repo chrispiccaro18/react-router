e9839a55e5a4f5471514b1f72746444e267c4af6
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-20 11:15:06 -0700

    added blue component

---
+++ src/components/Blue.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Blue() {
+  const divStyle = {
+    width: '500px',
+    height: '500px',
+    backgroundColor: 'blue'
+  };
+  return <div style={divStyle}></div>;
+}
