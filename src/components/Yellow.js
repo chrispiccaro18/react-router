b300f17e6b1d26f4028a1c80b66ce10fd811593c
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-20 11:15:32 -0700

    added yellow component

---
+++ src/components/Yellow.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Yellow() {
+  const divStyle = {
+    width: '500px',
+    height: '500px',
+    backgroundColor: 'yellow'
+  };
+  return <div style={divStyle}></div>;
+}
