659f68eb8464064d9e5fc4f54ea69ebcbdc2ddbd
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-20 11:39:20 -0700

    colors component

---
+++ src/components/Colors.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Colors({ match }) {
+  const color = match.params.color;
+  console.log(color);
+  const divStyle = {
+    width: '500px',
+    height: '500px',
+    backgroundColor: color
+  };
+  return <div style={divStyle}></div>;
+}
+
+Colors.propTypes = {
+  match: PropTypes.object.isRequired
+};
+
+export default Colors;
